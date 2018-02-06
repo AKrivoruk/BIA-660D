55e935fc22b9a1ec564e11ed02dcbfb05e699505
O885 <akrivoru@stevens.edu>
2018-02-06 18:42:39 -0500

    added hello world

--- Assignment Name/__init__.py
+++ Assignment Name/__init__.py
@@ -1,5 +1,5 @@
 print("Hello World")
-print "hello world"
+print("hello world")
 
 def muliten(thing):
     return thing * 10
@@ -9,4 +9,4 @@ def muliten(thing):
 x = "a string"
 y = 11.0
 
-print "this is an addition"
+print("this is an addition")
