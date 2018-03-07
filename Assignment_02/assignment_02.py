52504bd3269176cc2e99ceae659cd36c4d2af2ae
O885 <akrivoru@stevens.edu>
2018-03-07 15:55:56 -0500

    Q1, Q2, Q4, Q3, Q5

--- Assignment_02/assignment_02.py
+++ Assignment_02/assignment_02.py
@@ -111,6 +111,24 @@ def get_2015_site():
 
     pass
 
+def get_2014_site():
+    stats_header_bar = driver.find_element_by_class_name('megamenu-navbar-overflow__menu-item--stats')
+    random_delay()
+    stats_header_bar.click()
+    random_delay()
+    stats_line_items = stats_header_bar.find_elements_by_tag_name('li')
+    random_delay()
+    stats_line_items[0].click()
+    random_delay()
+
+    [li.text for li in stats_line_items]
+
+    reg_season_stats_2017 = select_element_by_text(stats_line_items, '2014 Regular Season Stats')
+    random_delay()
+    ActionChains(driver).move_to_element(reg_season_stats_2017).click().perform()
+    random_delay()
+    pass
+
 
 def select_AL():
     id_of_div = "sp_hitting-1"
@@ -166,7 +184,9 @@ def answer_question_one():
     select_HR()
     scrape_all()
     answer = data[0][0]
-    np.savetxt("answer_1.csv", answer, header = None)
+    print('this is the answer to q1')
+    print(answer)
+    np.savetxt("question_1.csv", answer, header = None)
     pass
 
 def select_first_inning():
@@ -182,6 +202,28 @@ def select_first_inning():
             random_delay()
             ActionChains(driver).move_to_element(first_inning).click().perform()
             random_delay()
+            pass
+        pass
+
+def select_all_star():
+    id_of_div = "sp_hitting-1"
+    al_div = driver.find_element_by_id(id_of_div)
+    options = al_div.get_attribute('innerHTML')
+    soup = bs4.BeasutifulSoup(options, "html5lib")
+    for opt in soup.fieldset.findall('optgroup'):
+        if opt.value == "Time Frame":
+            for option in opt.findall('option'):
+                if option.value == "preas":
+                ActionChains(driver).move_to_element(option).click().perform
+                random_delay()
+                first_inning = select_element_by_text(label, 'Pre All-Star')
+                random_delay()
+                ActionChains(driver).move_to_element(first_inning).click().perform()
+                random_delay()
+                    pass
+                pass
+            pass
+        pass
 
 def scrape_and_answer_question_2():
     select_AL()
@@ -262,7 +304,9 @@ def scrape_and_answer_question_2():
         pass
 
     Question_two_answers = [answer_2a , answer_2b]
-    np.savetxt("answer_2.csv", Question_two_answers, header=None)
+    print('this is the answer to q2')
+    print(Question_two_answers)
+    np.savetxt("question_2.csv", Question_two_answers, header=None)
     pass
 
 def get_full_name():
@@ -284,6 +328,23 @@ def get_full_name():
         return player_name
     pass
 
+def get_local(name)
+    data_div = driver.find_element_by_id('datagrid')
+    data_html = data_div.get_attribute('innerHTML')
+    soup = bs4.BeasutifulSoup(data_html, "html5lib")
+    ActionChains(driver).move_to_element(name).click().perform()
+    random_delay()
+    id_of_div = "player-vitals"
+    al_div = driver.find_element_by_id(id_of_div)
+    options = al_div.get_attribute('innerHTML')
+    soup = bs4.BeasutifulSoup(options, "html5lib")
+    for span in soup.findall('li'):
+        if span.id == 'Born':
+            player_local = span.text
+            return player_local
+        pass
+    pass
+
 def answer_question_four():
     scrape_all()
     random_delay()
@@ -291,7 +352,9 @@ def answer_question_four():
     pos = data[0][4]
     name = get_full_name()
     answer = [name, team, pos]
-    np.savetxt("answer_4.csv", answer, header=None)
+    print('this is the answer to q4')
+    print(answer)
+    np.savetxt("question_4.csv", answer, header=None)
     pass
 
 def type_in_name(name)
@@ -300,7 +363,7 @@ def type_in_name(name)
     active_player_search_input.send_keys(name)
     pass
 
-def answer_question_three()
+def answer_question_three():
     scrape_all()
     best_player = []
     bp_avg = 0
@@ -326,11 +389,27 @@ def answer_question_three()
                 three_b_answer = [player_full_name, row.Position]
                 pass
     question_answer = [three_a_answer, three_b_anser]
-    np.savetxt("answer_3.csv", question_answer, header=None)
+    print('this is the answer to q3')
+    print(question_answer)
+    np.savetxt("question_3.csv", question_answer, header=None)
     pass
 
-def answer_question_five()
+def answer_question_five():
+    get_2014_site()
+    select_all_star()
+    scrape_all()
+    answer = []
+    list_of_SA_nations = ['Argentina', 'Bolivia', 'Brazil', 'Chile', 'Colombia', 'Ecuador' 'Guyana', 'Paraguay', 'Peru', 'Suriname', 'Uruguay', 'Venezuela']
+    for row in data:
+        if get_local(row.Player) in list_of_SA_nations:
+            answer.append([get_full_name(row.Player)])
+            pass
+        pass
+    print('this is the answer for q5')
+    print(answer)
+    np.savetxt("question_5.csv", answer, header=None)
     pass
+
 def main():
     open_page()
     get_2015_site()
@@ -346,6 +425,7 @@ def main():
 
     answer_question_five(data)
     scrape_all()
+    select_all_star()
 
 if __name__ == '__main__':
     main()
